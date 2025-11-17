c88c17b0683f048b13098c3a5975968ae816b845
anksi <anksilae@gmail.com>
2025-11-17 09:29:36 +0200

    final

---
+++ Assets/Scripts/CleanupScene.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CleanupScene: MonoBehaviour
+{
+    void Start()
+    {
+        GameObject player = GameObject.Find("PlayerSpaceship1");
+        if (player != null)
+            Destroy(player);
+    }
+}
---
+++ Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAgain: MonoBehaviour
+{
+    public string levelName = "level-1"; 
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
